f148b9fcafb1d9ed569999b637980b011ec9bfaa
sbhusal123 <=>
2019-10-02 09:49:48 +0545

    Just configuring github.

--- basics/zip.py
+++ basics/zip.py
@@ -1,8 +1,12 @@
 numList = [19, 21, 46]
 strList = ['one', 'two', 'three']
+charList = ['@','&','*']
 
 outputA = zip(numList, strList)
 # print(list(outputA)) =  [(19, 'one'), (21, 'two'), (46, 'three')]
 
 outputB = zip(strList, numList)
 # print(list(outputB)) =   [('one', 19), ('two', 21), ('three', 46)]
+
+outputC = zip(numList,strList,charList)
+print(outputC)
